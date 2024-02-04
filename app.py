b72bfb6b491011b65f8cdebddd913f845b62f83f
nehasharma2513 <nehasharma2513@gmail.com>
2024-02-04 00:39:56 -0500

    Added skeleton for all html pages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import the dependencies.
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import pickle
 
 popular_df=pickle.load(open('Books-Recommendation-System\popular.pkl', 'rb'))
@@ -21,6 +21,24 @@ def index():
                            ratings_count=list(popular_df['count_ratings'].values),
                            rating=list(popular_df['avg_rating'].values)
                            )
+
+@app.route("/recommend_book_ui")
+def recommend_by_book_ui():
+    return render_template("recommend_by_book.html")
+
+@app.route("/recommend_book", methods=['post'])
+def recommend_book():
+    user_book=request.form.get("user_book")
+    return str(user_book)
+
+@app.route("/recommend_user_ui")
+def recommend_user_ui():
+    return render_template("recommend_by_user.html")
+
+@app.route("/recommend_user", methods=['post'])
+def recommend_user():
+    user_id=request.form.get("user_id")
+    return str(user_id)
 
 if __name__== "__main__":
     app.run(debug=True)
