96938aca453e1eeeed9029be246831d722d1d474
nehasharma2513 <nehasharma2513@gmail.com>
2024-02-05 10:38:56 -0500

    Attempt 1 - Getting user input

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 # Import the dependencies.
+import pandas as pd
 from flask import Flask, jsonify, render_template, request
+from sklearn.metrics.pairwise import cosine_similarity
 import pickle
 
 popular_df=pickle.load(open('Books-Recommendation-System\popular.pkl', 'rb'))
+rating_input_df=pickle.load(open('Books-Recommendation-System\rating_input.pkl', 'rb'))
+books_df=pickle.load(open('Books-Recommendation-System\books_df.pkl', 'rb'))
 #################################################
 # Flask Setup
 #################################################
@@ -29,6 +33,33 @@ def recommend_by_book_ui():
 @app.route("/recommend_book", methods=['post'])
 def recommend_book():
     user_book=request.form.get("user_book")
+    df_books_ratigs_user=rating_input_df.pivot_table(index='Book-Title', columns='User-ID', values='Book-Rating')
+    # filling n/a with 0 so far, assuming it means that no interest for a book by a user,
+    df_books_ratigs_user=df_books_ratigs_user.fillna(0)
+    # create a dictionary for mapping between row number ans Book-Title
+    index_title_dict=dict(df_books_ratigs_user.reset_index()['Book-Title'])
+    # apply cosine_similarity
+    books_similarity = cosine_similarity(df_books_ratigs_user)
+    # convert output of cosine_similarity into df
+    books_similarity_df=pd.DataFrame(books_similarity)
+    # introduce title here
+    books_similarity_df=books_similarity_df.rename(columns=index_title_dict)
+    books_similarity_df['Book-Title']=books_similarity_df.index
+    books_similarity_df['Book-Title']=books_similarity_df['Book-Title'].map(index_title_dict)
+    # find a similarity list for the book
+    recommendations=books_similarity_df[[user_book,'Book-Title']].sort_values(by=user_book, ascending=False)
+    book_title_list=[user_book]
+    recommendations=recommendations[~recommendations['Book-Title'].isin(book_title_list)]
+    # select top top_X_recommendations
+    top_recommendations=recommendations[:5].rename(columns={user_book:'similarity rate'})
+    recommendations_full_info=pd.merge(top_recommendations, books_df, left_on='Book-Title',right_on='Book-Title', how='left')
+    dict_years=dict(recommendations_full_info.groupby('Book-Title')['Year-Of-Publication'].max())
+    for i, row in recommendations_full_info.iterrows():
+        if row['Year-Of-Publication']!=dict_years[row['Book-Title']]:
+            recommendations_full_info.loc[i,'Year-Of-Publication']=0
+    recommendations_full_info=recommendations_full_info[recommendations_full_info['Year-Of-Publication'] != 0]
+    recommendations_full_info=recommendations_full_info.drop_duplicates(subset=['Book-Title'])
+    print(recommendations_full_info)
     return str(user_book)
 
 @app.route("/recommend_user_ui")
