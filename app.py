3bf69515e9cc09fd5f52d8348b7a21c69a82cb33
Valentyna K <uzarevich@ukr.net>
2024-02-08 21:52:32 -0500

    final

--- app.py
+++ app.py
@@ -4,10 +4,17 @@ from flask import Flask, jsonify, render_template, request
 from sklearn.metrics.pairwise import cosine_similarity
 import pickle
 
-popular_df=pickle.load(open('Books-Recommendation-System\popular.pkl', 'rb'))
-rating_input_df=pickle.load(open('Books-Recommendation-System/rating_input.pkl', 'rb'))
-books_df=pickle.load(open('Books-Recommendation-System/books_df.pkl', 'rb'))
-search_df=pickle.load(open('Books-Recommendation-System/search.pkl', 'rb'))
+popular_df=pickle.load(open('popular.pkl', 'rb'))
+rating_input_df=pickle.load(open('rating_input.pkl', 'rb'))
+books_df=pickle.load(open('books_df.pkl', 'rb'))
+search_df=pickle.load(open('search.pkl', 'rb'))
+ratings_df_all_cols=pickle.load(open('ratings_df_all_cols.pkl', 'rb'))
+ratings_df_algo_input=pickle.load(open('ratings_df_algo_input.pkl', 'rb'))
+books_df_algo_input=pickle.load(open('books_df_algo_input.pkl', 'rb'))
+
+#import model
+svd_default_model_mean = pickle.load(open('svd_default_model_mean.pickle', 'rb'))
+
 #################################################
 # Flask Setup
 #################################################
@@ -19,7 +26,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html",
+    return render_template("index.html" ,
                            book_name=list(popular_df['Book-Title'].values),
                            author=list(popular_df['Book-Author'].values),
                            image=list(popular_df['Image-URL-M'].values),
@@ -62,8 +69,8 @@ def recommend_book():
             recommendations_full_info.loc[i,'Year-Of-Publication']=0
     recommendations_full_info=recommendations_full_info[recommendations_full_info['Year-Of-Publication'] != 0]
     recommendations_full_info=recommendations_full_info.drop_duplicates(subset=['Book-Title'])
-    data = recommendations_full_info.to_dict('recommendations')
-    return render_template("recommend_by_book.html",data = data)
+    data = recommendations_full_info.to_dict('records')
+    return render_template("recommend_by_book.html",data = data, book=user_book)
 
 @app.route("/recommend_user_ui")
 def recommend_user_ui():
@@ -71,8 +78,46 @@ def recommend_user_ui():
 
 @app.route("/recommend_user", methods=['post'])
 def recommend_user():
-    user_id=request.form.get("user_id")
-    return str(user_id)
+    user_id=int(request.form.get("user_id"))
+        ## Find books prediction for a specific user and recommend top recommendations_count books
+    recommendations_count=4
+    model=svd_default_model_mean
+    # find those titles that we consider for predictions (e.g. not read by a user)
+    # find the books (titles) that were rated and presumably read by a user
+    rated_titles=[i for i in ratings_df_algo_input.loc[ratings_df_algo_input['User-ID']==user_id,'Book-Title']]
+    # find all the titles
+    all_titles=ratings_df_algo_input['Book-Title'].unique()
+    # separate those titles that were not read
+    titles_input_to_recommend=[i for i in all_titles if i not in rated_titles]
+
+    # find predictions for a user
+    predictions=[model.predict(uid=user_id, iid=i) for i in titles_input_to_recommend]
+    # get ratings estimate for books by the user
+    ratings=[i.est for i in predictions]
+    # convert predicted estimates by the user for not read books into df
+    pred_dict={
+        'Book-Title':titles_input_to_recommend,
+        'Estimated_Rate':ratings}
+    predictions_book=pd.DataFrame(pred_dict).sort_values('Estimated_Rate',ascending = False)
+    top_recommendations=predictions_book.head(recommendations_count)
+        
+    # populate books with full info, selecting those books with the most recent year of publication
+    recommendations_full_info=pd.merge(top_recommendations, books_df_algo_input, left_on='Book-Title',right_on='Book-Title', how='left')
+    dict_years=dict(recommendations_full_info.groupby('Book-Title')['Year-Of-Publication'].max())
+    for i, row in recommendations_full_info.iterrows():
+        if row['Year-Of-Publication']!=dict_years[row['Book-Title']]:
+            recommendations_full_info.loc[i,'Year-Of-Publication']=0
+    recommendations_full_info=recommendations_full_info[recommendations_full_info['Year-Of-Publication'] != 0]
+    recommendations_full_info=recommendations_full_info.drop_duplicates(subset=['Book-Title'])
+    data = recommendations_full_info.to_dict('records')
+     # find original user ratings
+    print(ratings_df_all_cols.columns)
+    u_data=ratings_df_all_cols[ratings_df_all_cols['User-ID']==user_id].sort_values(by='Book-Rating', ascending=False)
+    u_data=u_data[['Book-Title','Book-Rating','Book-Author','Publisher']]
+    if u_data.shape[0]>10:
+        u_data=u_data.head(10)
+    user_history = u_data.to_dict('records')
+    return render_template("recommend_by_user.html", data=data, datau=user_history, user_id=user_id)
 
 @app.route('/search', methods=['GET'])
 def view_books():
@@ -84,6 +129,10 @@ def view_books():
         books = search_df.to_dict('records')
 
     return render_template('search.html', books=books)
+
+@app.route('/tableau')
+def view_tableau():
+    return render_template('tableau.html')
 
 
 
